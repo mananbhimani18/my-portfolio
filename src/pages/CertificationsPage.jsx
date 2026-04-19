32f8602be81db037c42c6b6f295047c818c6e563
mananbhimani18 <mananbhimani24@gmail.com>
2026-04-19 15:32:37 +0530

    initial portfolio

--- src/pages/CertificationsPage.jsx
+++ src/pages/CertificationsPage.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { motion } from "framer-motion";
 import Certifications from "../sections/Certifications";
+
 function CertificationsPage() {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
-        <main>
-            <Certifications />
-        </main>
+        <motion.main
+            className="resume-page"  // 👈 IMPORTANT (reuse same layout feel)
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+        >
+            <div className="resume-container">
+                <Certifications />
+            </div>
+        </motion.main>
     );
 }
 
